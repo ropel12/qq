972e92429fe1076f508df698295af19fe33fca4f
satrio1701 <indogaming170@gmail.com>
2022-12-14 08:00:14 +0700

    p

--- src/Config.php
+++ src/Config.php
@@ -1,16 +1,16 @@
-<?php 
+<?php
+
 namespace Satrio\Scrapdata;
-class Config{
-    var $a =10;
-    var $b=11;
+
+class Config
+{
     public function __construct()
     {
-    $data = file(dirname(__FILE__).'/../.env',FILE_IGNORE_NEW_LINES);
-foreach($data as $conf){
-putenv($conf);
-}
+        $data = file(dirname(__FILE__) . '/../.env', FILE_IGNORE_NEW_LINES);
+        foreach ($data as $conf) {
+            putenv($conf);
+        }
     }
 }
 
 return new Config;
-
